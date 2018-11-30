70160fcb883ab29759528e3fe150a698cc65371a
Corban Riley <corbanbrook@gmail.com>
2018-11-30 11:59:33 -0500

    LightSystem uses passed in time

--- examples/cards-threejs/src/systems/LightsSystem.ts
+++ examples/cards-threejs/src/systems/LightsSystem.ts
@@ -2,26 +2,23 @@ import { System, EntityManager, Entity } from '../../../../src/ecs'
 import { Components } from '../components'
 import { Archetypes } from '../archetypes'
 
-let frame = 0
-
 export default class LightsSystem extends System<Components> {
-  update(manager: EntityManager<Components>, dt: number) {
+  update(manager: EntityManager<Components>, dt: number, time: number) {
     const { entities } = manager.getArchetype(Archetypes.Lights)
-    frame++
 
     entities.forEach((entity, idx) => {
       const light = entity.getComponent('light')
 
       if (idx % 2 === 0) {
         light.position.set(
-          Math.sin(frame / 100) * 10,
-          Math.cos(frame / 100) * 10,
+          Math.sin(time / 100) * 10,
+          Math.cos(time / 100) * 10,
           10
         )
       } else {
         light.position.set(
-          Math.cos(frame / 100) * 10,
-          Math.sin(frame / 100) * 10,
+          Math.cos(time / 100) * 10,
+          Math.sin(time / 100) * 10,
           10
         )
       }
