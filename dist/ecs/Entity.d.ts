ab6896f56f602af0bbc6f6e090ecf31b4ba71d63
Corban Riley <corbanbrook@gmail.com>
2020-03-03 10:39:07 -0500

    v0.5.1

--- dist/ecs/Entity.d.ts
+++ dist/ecs/Entity.d.ts
@@ -18,11 +18,11 @@ export default class Entity<C extends ComponentTypes> {
     onChange(listener: EntityChangeListener<C>): () => boolean;
     hasComponent(type: keyof C): boolean;
     has: (type: keyof C) => boolean;
-    hasComponents: (...types: string[]) => boolean;
+    hasComponents: (...types: (keyof C)[]) => boolean;
     addComponent: (component: C[keyof C]) => void;
     add: (component: C[keyof C]) => void;
-    removeComponent: (type: string) => void;
-    remove: (type: string) => void;
+    removeComponent: (type: keyof C) => void;
+    remove: (type: keyof C) => void;
     toggleComponent(componentClass: new (value: void) => ValueOf<C>, predicate: boolean): void;
     toggle: (componentClass: new (value: void) => C[keyof C], predicate: boolean) => void;
     getComponent<T extends keyof C>(type: T): C[T] | undefined;
