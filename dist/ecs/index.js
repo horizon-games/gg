3d8ded374f925cab358bac40ffcae9c8edfa4ccb
Corban Riley <corbanbrook@gmail.com>
2020-08-26 13:03:01 -0400

    v0.7.2

--- dist/ecs/index.js
+++ dist/ecs/index.js
@@ -1,15 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.World = exports.System = exports.EntityManager = exports.Archetype = exports.Entity = exports.Component = void 0;
 var Component_1 = require("./Component");
-Object.defineProperty(exports, "Component", { enumerable: true, get: function () { return Component_1.default; } });
+Object.defineProperty(exports, "Component", { enumerable: true, get: function () { return __importDefault(Component_1).default; } });
 var Entity_1 = require("./Entity");
-Object.defineProperty(exports, "Entity", { enumerable: true, get: function () { return Entity_1.default; } });
+Object.defineProperty(exports, "Entity", { enumerable: true, get: function () { return __importDefault(Entity_1).default; } });
 var Archetype_1 = require("./Archetype");
-Object.defineProperty(exports, "Archetype", { enumerable: true, get: function () { return Archetype_1.default; } });
+Object.defineProperty(exports, "Archetype", { enumerable: true, get: function () { return __importDefault(Archetype_1).default; } });
 var EntityManager_1 = require("./EntityManager");
-Object.defineProperty(exports, "EntityManager", { enumerable: true, get: function () { return EntityManager_1.default; } });
+Object.defineProperty(exports, "EntityManager", { enumerable: true, get: function () { return __importDefault(EntityManager_1).default; } });
 var System_1 = require("./System");
-Object.defineProperty(exports, "System", { enumerable: true, get: function () { return System_1.default; } });
+Object.defineProperty(exports, "System", { enumerable: true, get: function () { return __importDefault(System_1).default; } });
 var World_1 = require("./World");
-Object.defineProperty(exports, "World", { enumerable: true, get: function () { return World_1.default; } });
+Object.defineProperty(exports, "World", { enumerable: true, get: function () { return __importDefault(World_1).default; } });
 //# sourceMappingURL=index.js.map
